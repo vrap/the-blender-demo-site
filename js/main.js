5805337f8b442543dc65688e3565c8213ff04af1
vdaguenet <valentin.daguenet@gmail.com>
2014-06-17 00:05:32 +0200

    Button back to top design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(function() {
+    $('#toHome').hide();
 
+    $('#toAbout, #toHome').on('click', function(e){
+        e.preventDefault();
+        
+        if($(this).get(0).id == 'toAbout') {
+            var target = $('#about');
+        } else {
+            var target = $('#home');
+        }
+
+        $('html, body').stop().animate({
+           scrollTop: target.offset().top
+        }, 1000);
+    });
+
+    window.onscroll = function () {
+        if($('body').scrollTop() > 200) {
+            $('#toHome').fadeIn("slow");
+        } else {
+            $('#toHome').fadeOut('slow');
+        }
+    };
+
+});
